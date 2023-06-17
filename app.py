1098704b553e9cf26cd7a9f3afe669d72dbca5d2
kevcodes15 <136875458+kevcodes15@users.noreply.github.com>
2023-06-17 20:23:31 +0000

    added html and css

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask
+from flask import Flask,render_template
 app=Flask(__name__)
 #flask application created
 @app.route("/")
 def hello_world():
-  return "Hello, World"
-
+  return render_template('home.html')
 #use the following statement to run the code
 #0.0.0.0 is used to run locally
 if __name__=="__main__":
